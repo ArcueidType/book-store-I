949221b4d2d909d57b71b9d991c815c9bbe009b0
ArcueidType <981354012@qq.com>
2024-04-22 20:46:00 +0800

    fix the demo and pass forehead 60% test

--- be/model/user.py
+++ be/model/user.py
@@ -19,7 +19,7 @@ def jwt_encode(user_id: str, terminal: str) -> str:
         key=user_id,
         algorithm="HS256",
     )
-    return encoded.decode("utf-8")
+    return encoded
 
 
 # decode a JWT to a json string like:
